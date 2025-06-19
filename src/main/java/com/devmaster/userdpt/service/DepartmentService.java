361ab38e8a68c39f7c0ca46d25841edcbb648e13
felisbinojose <you@example.com>
2025-06-19 18:54:20 -0300

    feat(service): criado método de buscar department por id

--- src/main/java/com/devmaster/userdpt/service/DepartmentService.java
+++ src/main/java/com/devmaster/userdpt/service/DepartmentService.java
@@ -3,10 +3,12 @@ package com.devmaster.userdpt.service;
 import com.devmaster.userdpt.Dto.DepartmentDto;
 import com.devmaster.userdpt.entities.Department;
 import com.devmaster.userdpt.repositories.DepartmentRespository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DepartmentService {
@@ -23,4 +25,14 @@ public class DepartmentService {
     public List<Department> listarTodos(){
         return  departmentRespository.findAll();
     }
+
+    public Optional<Department> buscarPorId(Long id){
+        Optional<Department> department = departmentRespository.findById(id);
+        if (department.isEmpty()) {
+            throw new EntityNotFoundException("Department com o id:" + id + " não encontrado.");
+        }
+        return department;
+    }
+
+
 }
