e743facb9cce7ef7dfb5afe7540aa43eed84524f
felisbinojose <you@example.com>
2025-06-19 18:20:43 -0300

    fix: aplicando Lombok nas classes e conexão com database

--- src/main/java/com/devmaster/userdpt/entities/Department.java
+++ src/main/java/com/devmaster/userdpt/entities/Department.java
@@ -1,13 +1,19 @@
 package com.devmaster.userdpt.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
 
 @Entity
 @Table(name = "tb_department")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Department {
 
     @Id
@@ -15,24 +21,9 @@ public class Department {
     private Long id;
     private String name;
 
-    public Department() {
+    @OneToMany
+    private List<User> listaDeUsers;
 
-    }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
 }
--- src/main/java/com/devmaster/userdpt/entities/User.java
+++ src/main/java/com/devmaster/userdpt/entities/User.java
@@ -7,9 +7,18 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 
 @Entity
 @Table(name = "tb_user")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
     @Id
@@ -20,42 +29,7 @@ public class User {
 
     @ManyToOne
     @JoinColumn(name = "department_id")
-    private Department department;
+    private Department departmentId;
 
-    public User() {
-
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Department getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(Department department) {
-        this.department = department;
-    }
 
 }
