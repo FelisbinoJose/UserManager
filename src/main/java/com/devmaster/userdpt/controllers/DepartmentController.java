2abe3c85113a9f8619475f5e33b9a630b493a213
felisbinojose <you@example.com>
2025-06-19 18:34:47 -0300

    feat(controller): criado endpoint de listar todos os department

--- src/main/java/com/devmaster/userdpt/controllers/DepartmentController.java
+++ src/main/java/com/devmaster/userdpt/controllers/DepartmentController.java
@@ -6,10 +6,9 @@ import com.devmaster.userdpt.entities.Department;
 import com.devmaster.userdpt.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/department")
@@ -22,6 +21,12 @@ public class DepartmentController {
     public ResponseEntity<Department> criarDepartment(@RequestBody DepartmentDto departmentDto){
         Department departmentCriado = departmentService.criarDepartment(departmentDto);
         return ResponseEntity.ok(departmentCriado);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Department>> listarTodos(){
+        List<Department> listaDeDepartment = departmentService.listarTodos();
+        return ResponseEntity.ok(listaDeDepartment);
     }
 
 }
